c7257ad5a279c3a58798b80792ab07de2a97f955
devseriouslyy <157292575+devseriouslyy@users.noreply.github.com>
2026-01-11 00:17:33 +0530

    Update hello_world.rs

--- hello_world.rs
+++ hello_world.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, Aditya!");
 }
+
+//fn main() - It is the program’s entry point for an executable.
+//Similar to int main() in C language 
+//In real Rust code, main is usually tiny.
